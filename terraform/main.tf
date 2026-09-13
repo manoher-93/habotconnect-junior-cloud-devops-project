a9daca6739f9e1daf85f6daa913c3660b92becd9
Manoher Singh <manoharmsp93@gmail.com>
2026-09-13 23:13:17 +0530

    Fix Terraform formatting

--- terraform/main.tf
+++ terraform/main.tf
@@ -14,7 +14,7 @@ resource "google_storage_bucket" "d0_raw_landing" {
   name                        = var.bucket_name
   location                    = var.region
   uniform_bucket_level_access = true
-  public_access_prevention   = "enforced"
+  public_access_prevention    = "enforced"
   force_destroy               = false
 
   versioning {
